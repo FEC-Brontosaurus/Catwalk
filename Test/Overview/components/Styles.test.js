7ff3866000c9bd10c97fdc01b1af86e22a44e922
Aaron <aaronfink02@gmail.com>
2021-03-02 14:37:28 -0800

    Completed the User Selection 1.1.2.4. Implemented basic test. Reformatted the styles and added a styles row to account for testing behavior. Added a property to package.json to allow jest to not throw errors when encountering static images.

--- Test/Overview/components/Styles.test.js
+++ Test/Overview/components/Styles.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render, fireEvent, queryByAttribute, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Styles from '../../../client/components/Overview/components/Styles.jsx';
+import StylesRow from '../../../client/components/Overview/components/StylesRow.jsx';
 import axios from 'axios';
 
 const getById = queryByAttribute.bind(null, 'id');
 
 //  Test to see if each component is successfully rendering to the DOM
-//  NOTE: MUST ADD NEW ELEMENTS AS THEY ARE CREATED TO TEST IF THEY RENDER
 describe('componentsRendingToTheDOM', () => {
   it('Default style should be first in the list', () => {
     //  create a dummy current product that we can use to pass into funcitons
@@ -25,5 +25,39 @@ describe('componentsRendingToTheDOM', () => {
 
     //  check to make sure that the first style is the current style
     expect(currentStyle.style_id).toBe(styles[0].style_id);
+  })
+
+  it('Should update the current style when clicking on a style in the thumbnail', async () => {
+
+    //  create dummy variables to simulate render
+    const setCurrentStyle = jest.fn();
+    const currentStyle = {style_id: 110041, name: "Cyan", photos: ['test1']};
+    const row = [
+      {style_id: 110041, name: "Cyan", photos: [{thumbnail_url: 'test1'}]},
+      {style_id: 110042, name: "Blue", photos: [{thumbnail_url: 'test2'}]},
+      {style_id: 110043, name: "Purple", photos: [{thumbnail_url: 'test3'}]},
+      {style_id: 110044, name: "Red", photos: [{thumbnail_url: 'test4'}]}]
+
+    //  simulate what is rendering to the screen
+    const { findByTestId } = render(
+    <div>
+      {row.map((style, styleidx) => (
+      <div id="style-div" key={style.name + styleidx} >
+        <img
+          id="style-img"
+          style={(style.style_id === currentStyle.style_id) ? {boxShadow: '0px 0px 5px 3px #888888'} : null }
+          src={style.photos[0].thumbnail_url}
+          key={style.name + styleidx}
+          onClick={() => setCurrentStyle(style) }
+          data-testid={style.name}
+        />
+      </div>
+    ))}
+    </div>)
+
+    //  simulate a click on one of the images
+    await fireEvent.click(screen.getByTestId("Blue"));
+    //  expect the setCurrentState to be called after click
+    await expect(setCurrentStyle).toBeCalled();
   })
 });
