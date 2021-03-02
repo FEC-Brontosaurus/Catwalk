7c20ec912f0a286d55f03851dffd05682ae6be7e
Bryan Clark <clark.bryanr@gmail.com>
2021-03-01 16:14:40 -0800

    added initial wireframe of QandA module using divs. zero functionality, although the hardcoded divs render to the DOM

--- client/components/QandA/QandA.jsx
+++ client/components/QandA/QandA.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const QandA = (props) => (
-  <div id="QandA" >Question and Answer </div>
+  <div id="QandA">
+    <div id="QandA-Search"> SEARCH FOR QUESTIONS </div>
+    <div className="QandA-question">Q: <span> random text </span> <span className="helpful-yes"> Helpful? <span href="#">yes</span></span> <span className="add-an-answer"> Add an Answer + </span></div>
+    <div className="QandA-Answer"> A: Random text answer <span className="username-timestamp"> </span> <span className="helpful-yes"> Helpful? <span href="#"> yes </span> <span className="report-answer"> Report </span></span></div>
+    <div className="QandA-question">Q: <span> random text </span> <span className="helpful-yes"> Helpful? <span href="#">yes</span></span> <span className="add-an-answer"> Add an Answer + </span></div>
+    <div className="QandA-Answer"> A: Random text answer <span className="username-timestamp"> </span> <span className="helpful-yes"> Helpful? <span href="#"> yes </span> <span className="report-answer"> Report </span></span></div>
+    <div className="QandA-question">Q: <span> random text </span> <span className="helpful-yes"> Helpful? <span href="#">yes</span></span> <span className="add-an-answer"> Add an Answer + </span></div>
+    <div className="QandA-Answer"> A: Random text answer <span className="username-timestamp"> </span> <span className="helpful-yes"> Helpful? <span href="#"> yes </span> <span className="report-answer"> Report </span></span></div>
+    <div className="QandA-question">Q: <span> random text </span> <span className="helpful-yes"> Helpful? <span href="#">yes</span></span> <span className="add-an-answer"> Add an Answer + </span></div>
+    <div className="QandA-Answer"> A: Random text answer <span className="username-timestamp"> </span> <span className="helpful-yes"> Helpful? <span href="#"> yes </span> <span className="report-answer"> Report </span></span></div>
+    <form>
+      <button type="button"> More Answered Questions </button>
+      <button type="button"> Add a Question </button>
+    </form>
+  </div>
 );
 
 export default QandA;
