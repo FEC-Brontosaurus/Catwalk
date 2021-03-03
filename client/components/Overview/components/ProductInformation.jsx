6af0674a5c7ff6a8c1642b341202d109fc874d4f
Aaron <aaronfink02@gmail.com>
2021-03-02 19:09:49 -0800

    Rendering product: category, title, product, overview, share, and price. Price displays the default price but if there is a sale price default price has a line through and sale price is red. Tests are working for all these. Reformatted a lot of components to make testing easier.

--- client/components/Overview/components/ProductInformation.jsx
+++ client/components/Overview/components/ProductInformation.jsx
@@ -4,13 +4,29 @@ import facebook from '../../../../public/static/facebook.png';
 import twitter from '../../../../public/static/twitter.png';
 import pinterest from '../../../../public/static/pinterest.png';
 
+//  function to render the general information to the DOM
+//  the price is rendered in a different way however
+//  if there is not a current price don't render anything
+//  if there is a current price but no sale price render the current price
+//  if there is botha current price and sale price render the sale price in red followed by the current price with a line through it
 const ProductInformation = ({ currentProduct, currentStyle }) => (
   <div id="ProductInformation">
-    {console.log(currentStyle)}
     <div id="product-info-star-rating">Star Rating Placeholder</div>
     <div id="product-info-category">Category: {currentProduct.category}</div>
     <div id="product-info-title">Title: {currentProduct.name}</div>
-    <div id="product-info-price">Price: {currentProduct.default_price}</div>
+
+    {currentStyle ? currentStyle.sale_price ?
+    <div style={{display: 'inline-block'}}>
+      <div id="product-info-price-discount" style={{color: 'red', display: 'inline-block'}}>
+        {currentStyle.sale_price}
+      </div>
+      <div id="product-info-price-line" style={{textDecoration: 'line-through', display: 'inline-block'}}>
+        {currentProduct.default_price}
+      </div>
+    </div> :
+    <div id="product-info-price">Price: {currentProduct.default_price}</div> :
+    <div id="product-info-price">Price: {currentProduct.default_price}</div>}
+
     <div id="product-info-overview">Overview: {currentProduct.description}</div>
     <div id="product-info-share">
       <img id="product-info-share-facebook" src={facebook} alt="" />
