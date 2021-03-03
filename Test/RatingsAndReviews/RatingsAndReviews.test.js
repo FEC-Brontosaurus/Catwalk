fd830d1bc0695e7d8587178523537b88a32733fc
Greta Schock <gschockcodes@gmail.com>
2021-03-02 19:25:11 -0800

    Changes to all RatingAndReview files (commit before testing for the video)

---
+++ Test/RatingsAndReviews/RatingsAndReviews.test.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { render, fireEvent, queryByAttribute } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import RatingsAndReviews from '../../client/components/RatingsAndReviews/RatingsAndReviews.jsx';
+
+// const getById = queryByAttribute.bind(null, 'id');
+// const getByClass = queryByAttribute.bind(null, 'class');
+
+// //Test to see if each component is successfully rendering to the DOM
+// //As we add new elements to the DOM, we have to create a new test to makes sure they render
+// describe('Ratings And Reviews Tests', () => {
+//     it ('Should render Rating and Review components to the DOM', () => {
+//         //create a dummy current product that we can use to pass into functions
+//         const currentProduct = {id: '1111'};
+
+//         //get the div that is inside of the Rating and Reviews function 
+//         const { container } = render(<RatingsAndReviews currentProduct={currentProduct} />);
+
+//         //get the div we want to check by it's id
+//         const IndividualReviewTile = getById(container, 'IndividualReviewTile-div');
+
+//         //check to make sure each div was added to the DOM
+//         expect(IndividualReviewTile).toBeInTheDocument();
+//     });
+// });
