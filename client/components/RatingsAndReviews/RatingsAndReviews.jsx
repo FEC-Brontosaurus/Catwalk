10fc2152bb16abc977056250cc8b5adab8806938
Greta Schock <gschockcodes@gmail.com>
2021-03-02 19:30:15 -0800

    Added all relevant files for RatingsAndReviews (this has not be tested, just for video purposes)`

--- client/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+import IndividualReviewTile from './components/IndividualReviewTile.jsx';
 
-const RatingsAndReviews = (props) => (
+const RatingsAndReviews = ({ currentProduct }) => {
+    const [productReviewArr, setProductReviewArr] = useState([]);
+
+//given the id from the current product, make an API GET request 
+const getAllReviews = () => {
+    console.log('currentProduct id: ', currentProduct.id)
+    axios.get('http://localhost:3000/api/getAllReviews', {params: {'id': currentProduct.id}})
+      .then((results) => {
+        console.log('results on client side: ', results.data);
+        setProductReviewArr(results.data)
+      })
+      .catch((err) => console.log(err));
+}
+
+useEffect(() => (currentProduct ? getAllReviews() : null), [currentProduct]);
+
+
+
+return (
     <div id="RatingsAndReviews" >
-        <div>Ratings And Reviews</div>
+        <h3>Ratings and Reviews</h3>
+        {productReviewArr.map((productReviewObj) => (
+            <IndividualReviewTile
+              key={productReviewObj.review_id}
+              productReviewObj={productReviewObj}
+            />
+        )
+        )}
     </div>
-    
-);
+)};
 
 export default RatingsAndReviews;
