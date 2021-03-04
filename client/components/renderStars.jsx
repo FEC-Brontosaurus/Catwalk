3b8d5c9eff5d09371d06ed058cf012198892f4e0
Greta Schock <gschockcodes@gmail.com>
2021-03-04 10:32:43 -0800

    Added CSS files (just in case save before I try pulling from the main again)

---
+++ client/components/renderStars.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const renderStars = () => {
+    //I: a number 
+    //O: an HTML element 
+
+};
+
+export default renderStars;
