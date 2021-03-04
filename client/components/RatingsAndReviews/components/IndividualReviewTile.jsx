458f3910b0a1ca9164b3879b21b4bf89cd2057a8
Greta Schock <gschockcodes@gmail.com>
2021-03-03 17:38:35 -0800

    1.2.2.4 Added toggle button feature depending on body string length

--- client/components/RatingsAndReviews/components/IndividualReviewTile.jsx
+++ client/components/RatingsAndReviews/components/IndividualReviewTile.jsx
@@ -34,50 +34,28 @@ const IndividualReviewTile = ({ productReviewObj }) => {
     }
 
 
-    var test = 'THIS IS A TEST TO MAKE SURE THAT THE BUTTON RENDERS THE INFORMATION WE NEED: Sapiente repudiafjaslkfjdlkfjskldfjksdlfjdskfjdsn;jiopra;vrewi;cmrjeowiruxiewruthrjrjhbrekriwhjrknejwdidflekndae laudantium numquam dolor. Sit in sint sint quae harum amet. Magnam incidunt quaerat dolores dignissimos ipsum sed. Quas facilis nesciunt aut. Quod eligendi veritatis quam nisi quasi vel temporibus nesciunt quaefhdjskfhsjkfhdsjfhdsjkfdshf';
+    // var test = 'THIS IS A TEST TO MAKE SURE THAT THE BUTTON RENDERS THE INFORMATION WE NEED: Sapiente repudiafjaslkfjdlkfjskldfjksdlfjdskfjdsn;jiopra;vrewi;cmrjeowiruxiewruthrjrjhbrekriwhjrknejwdidflekndae laudantium numquam dolor. Sit in sint sint quae harum amet. Magnam incidunt quaerat dolores dignissimos ipsum sed. Quas facilis nesciunt aut. Quod eligendi veritatis quam nisi quasi vel temporibus nesciunt quaefhdjskfhsjkfhdsjfhdsjkfdshf';
 
     const reformattedBody = () => {
-        //if isShowingFullReviewBody is true
         if (isShowingFullReviewBody) {
-            //change the test to show the full string given 
-            return test;
+            return productReviewObj.body;
         } else {
-          //if isShowingFullReviewBody is false 
-            //change the test to show only the first 250 characters of the test string 
-            var shortenedStr = test.slice(0, 250);
+            var shortenedStr = productReviewObj.body.slice(0, 250);
             return shortenedStr + '...';
         }
-
-    const reformattedReviewBodyButton = () => {
-        if (isShowingFullReviewBody) {
-            return 'Show Less';
-        } else {
-            return 'Show More';
-        }
     }
-        
 
-
-    }
     return (
         <div id="IndividualReviewTile-div">
             <div>{reformattedDate(productReviewObj.date)}</div>
             <div><strong>{reformattedSummary(productReviewObj.summary)}</strong></div>
-
-            {test.length >= 250 
-              ? <div>{reformattedBody(test)}
-                  <button onClick={(event) => {
-                    console.log("I have been clicked! :)", 'current isShowingFullReviewBody state: ', isShowingFullReviewBody);
-                    setIsShowingFullReviewBody(!isShowingFullReviewBody);
-                    }}>Show More</button>
-              </div> 
-              : <div>{test}</div>
-            }
-            {/* {productReviewObj.body.length >= 250 
-              ? <div>{test}<button>Show More</button></div> 
+            {productReviewObj.body.length >= 250 
+              ? <div>{reformattedBody(productReviewObj.body)}
+                  <button onClick={(event) => {setIsShowingFullReviewBody(!isShowingFullReviewBody)}}
+                  >{isShowingFullReviewBody ? `Show Less` : 'Show More'}</button>
+                </div> 
               : <div>{productReviewObj.body}</div>
-            } */}
-
+            }
             {productReviewObj.photos.length > 0 
             && productReviewObj.photos.map((photoObj) => (
                 <img
@@ -87,7 +65,6 @@ const IndividualReviewTile = ({ productReviewObj }) => {
                   alt="Individual Review Tile Photo"
                 />
             ))}
-            <br></br>
         </div>
     )
 }
