3a74dd3b0a419c6df9a39ec4b8a7f48c15340492
Greta Schock <gschockcodes@gmail.com>
2021-03-04 16:04:16 -0800

    Added RenderStars component

--- client/components/RatingsAndReviews/components/IndividualReviewTile.jsx
+++ client/components/RatingsAndReviews/components/IndividualReviewTile.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable quote-props */
 import React, { useState } from 'react';
-// eslint-disable-next-line import/extensions
 import Modal from '../Modals/modalIndex.jsx';
+import RenderStars from '../../renderStars.jsx'
 import styles from '../styles/RatingsAndReviewsStyles.css';
 
 const IndividualReviewTile = ({ productReviewObj }) => {
@@ -43,7 +41,6 @@ const IndividualReviewTile = ({ productReviewObj }) => {
   const reformattedBody = () => {
     if (isShowingFullReviewBody) {
       return productReviewObj.body;
-    // eslint-disable-next-line no-else-return
     } else {
       const shortenedStr = productReviewObj.body.slice(0, 250);
       return `${shortenedStr}...`;
@@ -52,7 +49,7 @@ const IndividualReviewTile = ({ productReviewObj }) => {
 
   return (
     <div id="IndividualReviewTile-div">
-      <div>{productReviewObj.rating}</div>
+      <RenderStars rating={productReviewObj.rating}/>
       <div>{reformattedDate(productReviewObj.date)}</div>
       <div><strong>{reformattedSummary(productReviewObj.summary)}</strong></div>
       {productReviewObj.body.length >= 250
--- client/components/renderStars.jsx
+++ client/components/renderStars.jsx
@@ -1,10 +1,18 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 
-const renderStars = () => {
-    //I: a number 
-    //O: an HTML element 
+//A function that given a number (can be non-integer) will produce an HTML element to be inserted into the DOM
+//Styling is in public/styles.css
 
+const RenderStars = ({rating}) => {
+    const starTotal = 5;
+    const starPercentage = (rating / starTotal)*100;
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+    return (
+        <div className="stars-outer">
+            <div className="stars-inner" style={{width: starPercentageRounded}}></div>
+        </div>
+    )
 };
 
-export default renderStars;
+export default RenderStars;
