9025b07fa4b427a13f4bc75b09e4667850f4e9d5
Greta Schock <gschockcodes@gmail.com>
2021-03-05 19:39:08 -0800

    Added code to transfer metadata to the front end

--- client/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -5,6 +5,7 @@ import IndividualReviewTile from './components/IndividualReviewTile.jsx';
 
 const RatingsAndReviews = ({ currentProduct }) => {
   const [productReviewArr, setProductReviewArr] = useState([]);
+  const [productMetadataObj, setProductMetadataObj] = useState({});
 
   // given the id from the current product, make an API GET request
   const getAllReviews = () => {
@@ -15,11 +16,22 @@ const RatingsAndReviews = ({ currentProduct }) => {
       .catch((err) => console.log(err));
   };
 
+  //given the id from the current product, make an API GET request to get the review metadata for a given product
+  const getProductMetaData = () => {
+    axios.get('http://localhost:3000/api/getProductMetadata', { params: { 'id': currentProduct.id } })
+      .then((results) => {
+        setProductMetadataObj(results.data)
+      })
+      .catch((err) => console.log(err));
+  }
+
   useEffect(() => (currentProduct ? getAllReviews() : null), [currentProduct]);
+  useEffect(() => (currentProduct ? getProductMetaData() : null), [currentProduct]);
 
   return (
     <div id="RatingsAndReviews">
       <h3>Ratings and Reviews</h3>
+      {console.log('in return statement: ', productMetadataObj)}
       {productReviewArr.map((productReviewObj) => (
         <IndividualReviewTile
           key={productReviewObj.review_id}
