54697182afc1a8bd1ab6510997602dddce8a2c67
Greta Schock <gschockcodes@gmail.com>
2021-03-07 20:13:43 -0800

    Added star buttons to modal

--- client/components/RatingsAndReviews/Modals/AddReviewModal.jsx
+++ client/components/RatingsAndReviews/Modals/AddReviewModal.jsx
@@ -16,12 +16,26 @@ const AddReviewModal = () => {
     }
 
     return (
-    <div>
+      <div>
         <button type="button "id="myBtn" onClick={() => openReviewModal(reviewModalRef)}>Open Modal</button>
         <div ref={reviewModalRef} id="myModal" class="modal">    
             <div class="modal-content">            
             <span class="close" onClick={() => closeReviewModal(reviewModalRef)}>&times;</span>
             <h3>Add Review</h3>
+            <table>
+                <tbody>
+                <tr>
+                    <td><strong>Overall Rating</strong></td>
+                    <td>
+                        <button>Poor &#9733;</button>
+                        <button>Fair &#9733;&#9733;</button>
+                        <button>Average &#9733;&#9733;&#9733;</button>
+                        <button>Good &#9733;&#9733;&#9733;&#9733;</button>
+                        <button>Great &#9733;&#9733;&#9733;&#9733;&#9733;</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
             </div>
         </div>
       </div>
