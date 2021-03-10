ed5f261cba4c48dcdcd669cd176f890dd82967ed
Greta Schock <gschockcodes@gmail.com>
2021-03-09 23:07:46 -0800

    Added barebones for testing

--- Test/RatingsAndReviews/RatingsAndReviews.test.js
+++ Test/RatingsAndReviews/RatingsAndReviews.test.js
@@ -1,26 +1,55 @@
-// import React from 'react';
-// import { render, fireEvent, queryByAttribute } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-// import RatingsAndReviews from '../../client/components/RatingsAndReviews/RatingsAndReviews.jsx';
-
-// const getById = queryByAttribute.bind(null, 'id');
-// const getByClass = queryByAttribute.bind(null, 'class');
+import React from 'react';
+import { render, queryByAttribute } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import RatingsAndReviews from '../../client/components/RatingsAndReviews/RatingsAndReviews.jsx'
 
 
-// //Test to see if each component is successfully rendering to the DOM
-// //As we add new elements to the DOM, we have to create a new test to makes sure they render
-describe('Ratings And Reviews Tests', () => {
-    it ('Should render Rating and Review components to the DOM', () => {
-    //         //create a dummy current product that we can use to pass into functions
-    //         const currentProduct = {id: '1111'};
+const getById = queryByAttribute.bind(null, 'id');
 
-    //         //get the div that is inside of the Rating and Reviews function
-    //         const { container } = render(<RatingsAndReviews currentProduct={currentProduct} />);
+//  Test to see if each component is successfully rendering to the DOM
+//  NOTE: MUST ADD NEW ELEMENTS AS THEY ARE CREATED TO TEST IF THEY RENDER
+describe('Rating and Reviews Tests', () => {
+  it('Should render RatingsAndReviews component to the DOM', () => {
+    //  create a dummy current product/style that we can use to pass into funcitons
+    const currentProduct = { id: '1111' };
 
-    //  const IndividualReviewTileTest = getById(container, 'IndividualReviewTile-div');
+    //  get the div that is inside of the overview function
+    const { container } = render(<RatingsAndReviews currentProduct={currentProduct} />);
 
-    //         //check to make sure each div was added to the DOM
-    //         expect(IndividualReviewTile).toBeInTheDocument();
-    expect(1).toBe(1);
+    //  get the div we want to check by it's id
+    const RatingsAndReviews = getById(container, 'RatingsAndReviews-wrapper');
+
+    //  check to make sure the div was added to the DOM
+    expect(RatingsAndReviews).toBeInTheDocument();
   });
+
+  it('Should render ProductBreakdown component to the DOM', () => {
+    //  create a dummy current product/style that we can use to pass into funcitons
+    const currentProductMetadataObj = {
+      product_id: 12345,
+      rating: 0,
+      summary: "beep boop hello its me its a tre",
+      body: "meep morp twinkle twinkle little star I would like a brand new car",
+      recommend: true,
+      name: "gretamarieschock",
+      email: "pleasework@gmail.com",
+      photos: [],
+      characteristics: {67537: 5, 67535: 5, 67536: 5, 67538: 5}
+   } ;
+
+    //  get the div that is inside of the overview function
+    const { container } = render(<ProductBreakdown currentProductMetadataObj={currentProductMetadataObj} />);
+
+    //  get the div we want to check by it's id
+    const ProductBreakdown = getById(container, 'product-breakdown');
+
+    //  check to make sure the div was added to the DOM
+    expect(ProductBreakdown).toBeInTheDocument();
+  });
+
+
+
 });
+
+
+
