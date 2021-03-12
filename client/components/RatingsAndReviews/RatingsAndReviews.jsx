8506fc2205ad9a05970d6675ac1c9a348382c50b
Greta Schock <gschockcodes@gmail.com>
2021-03-12 11:55:03 -0800

    Removed console.logs

--- client/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -46,9 +46,6 @@ const RatingsAndReviews = ({ currentProduct, setOverviewAverage, reviewScroll, L
   const renderTwoMoreReviewTiles = () => {
     if (productReviewArr.length !== constantReviewArr.length) {
       const elementsToBeAddedToDisplay = constantReviewArr.slice(currentProductReviewArrIndex, (currentProductReviewArrIndex + 2));
-      console.log('productReviewArr: ', productReviewArr)
-      console.log('constantReviewArr: ', constantReviewArr)
-      console.log(elementsToBeAddedToDisplay);
       setProductReviewArr(productReviewArr.concat(elementsToBeAddedToDisplay));
       setCurrentProductReviewArrIndex(currentProductReviewArrIndex + 2);
     } else {
